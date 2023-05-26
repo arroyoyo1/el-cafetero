aa74e8a1bee5e35886f248457e0b04088cba8732
Mauricio Arroyo <arroyoyo@outlook.com>
2023-05-26 14:44:17 -0600

    TerceraEntrega - MauricioArroyo

--- main.js
+++ main.js
@@ -1,93 +1,165 @@
-//segunda pre-entrega en inglés pq me estoy tomando un cursito y me siento bilinnnngue
 
-let parkName = prompt("enter the name of the amusement park:");
-let numRides = prompt("enter the number of rides in the park:");
-let ticketPrice = prompt("enter the price of a ticket:");
+const cartContainer = document.querySelector('.cart-container');
+const productList = document.querySelector('.product-list');
+const cartList = document.querySelector('.cart-list');
+const cartTotalValue = document.getElementById('cart-total-value');
+const cartCountInfo = document.getElementById('cart-count-info');
+let cartItemID = 1;
 
-let ridesPerDay = Math.floor(numRides / 2.5);
-let revenuePerDay = 0;
-let parkInfo = {name: parkName, rides: numRides, ticketPrice: ticketPrice};
+eventListeners();
 
 
-function addRevenue(revenue, price) {
-  return revenue + price;
+function eventListeners(){
+    window.addEventListener('DOMContentLoaded', () => {
+        loadJSON();
+        loadCart();
+    });
+
+    document.querySelector('.navbar-toggler').addEventListener('click', () => {
+        document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
+    });
+
+
+    document.getElementById('cart-btn').addEventListener('click', () => {
+        cartContainer.classList.toggle('show-cart-container');
+    });
+
+
+    productList.addEventListener('click', purchaseProduct);
+
+
+    cartList.addEventListener('click', deleteProduct);
 }
-revenuePerDay = addRevenue(revenuePerDay, ticketPrice * 100);
 
 
-let infoString = `welcome to ${parkInfo.name}! we have ${parkInfo.rides} rides and the ticket price is $${parkInfo.ticketPrice}.`;
+function updateCartInfo(){
+    let cartInfo = findCartInfo();
+    cartCountInfo.textContent = cartInfo.productCount;
+    cartTotalValue.textContent = cartInfo.total;
+}
 
 
-let ridesWithRestrictions = Math.floor(numRides * 0.2);
+function loadJSON(){
+    fetch('furniture.json')
+    .then(response => response.json())
+    .then(data =>{
+        let html = '';
+        data.forEach(product => {
+            html += `
+                <div class = "product-item">
+                    <div class = "product-img">
+                        <img src = "${product.imgSrc}" alt = "product image">
+                        <button type = "button" class = "add-to-cart-btn">
+                            <i class = "fas fa-shopping-cart"></i>Add To Cart
+                        </button>
+                    </div>
+                    <div class = "product-content">
+                        <h3 class = "product-name">${product.name}</h3>
+                        <span class = "product-category">${product.category}</span>
+                        <p class = "product-price">$${product.price}</p>
+                    </div>
+                </div>
+            `;
+        });
+        productList.innerHTML = html;
+    })
+    .catch(error => {
+        alert(`User live server or local server`);
+        
+    })
+}
 
 
-let rideNames = ["roller coaster", "carousel", "ferris wheel", "teacups", "water ride"];
+function purchaseProduct(e){
+    if(e.target.classList.contains('add-to-cart-btn')){
+        let product = e.target.parentElement.parentElement;
+        getProductInfo(product);
+    }
+}
 
 
-let rideWithLongestName = rideNames.find(function(ride) {
-  return ride.length > 10;
-});
+function getProductInfo(product){
+    let productInfo = {
+        id: cartItemID,
+        imgSrc: product.querySelector('.product-img img').src,
+        name: product.querySelector('.product-name').textContent,
+        category: product.querySelector('.product-category').textContent,
+        price: product.querySelector('.product-price').textContent
+    }
+    cartItemID++;
+    addToCartList(productInfo);
+    saveProductInStorage(productInfo);
+}
 
+function addToCartList(product){
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    cartItem.setAttribute('data-id', `${product.id}`);
+    cartItem.innerHTML = `
+        <img src = "${product.imgSrc}" alt = "product image">
+        <div class = "cart-item-info">
+            <h3 class = "cart-item-name">${product.name}</h3>
+            <span class = "cart-item-category">${product.category}</span>
+            <span class = "cart-item-price">${product.price}</span>
+        </div>
+        <button type = "button" class = "cart-item-del-btn">
+            <i class = "fas fa-times"></i>
+        </button>
+    `;
+    cartList.appendChild(cartItem);
+}
 
-let popularRides = rideNames.filter(function(ride) {
-  return ride !== "teacups";
-});
+function saveProductInStorage(item){
+    let products = getProductFromStorage();
+    products.push(item);
+    localStorage.setItem('products', JSON.stringify(products));
+    updateCartInfo();
+}
 
+function getProductFromStorage(){
+    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+}
 
-alert(`on an average day, we have about ${ridesPerDay} rides operating in ${parkInfo.name} and earn $${revenuePerDay} in revenue.`);
-alert(infoString);
-alert(`approximately ${ridesWithRestrictions} of our rides have restrictions.`);
-alert(`the ride with the longest name is ${rideWithLongestName}.`);
-alert(`our popular rides include ${popularRides.join(", ")}.`);
+function loadCart(){
+    let products = getProductFromStorage();
+    if(products.length < 1){
+        cartItemID = 1;
+    } else {
+        cartItemID = products[products.length - 1].id;
+        cartItemID++;
+    }
+    products.forEach(product => addToCartList(product));
+ 
+    updateCartInfo();
+}
 
+function findCartInfo(){
+    let products = getProductFromStorage();
+    let total = products.reduce((acc, product) => {
+        let price = parseFloat(product.price.substr(1)); 
+        return acc += price;
+    }, 0);
 
+    return{
+        total: total.toFixed(2),
+        productCount: products.length
+    }
+}
 
-//primera pre-entrega
-// function coffeeOrder() {
-//     let order = "";
-//     let totalPrice = 0;
-  
-//     while (order !== "salir") {
-//       order = prompt(
-//         "bienvenidx al cafetero, escribe tu orden del día (espresso, latte, cappuccino, americano, salir):"
-//       );
-  
-//       switch (order) {
-//         case "espresso":
-//         case "espreso":
-//           totalPrice += 45.50;
-//           alert("buena elección; tu espresso está siendo preparado :)");
-//           break;
-  
-//         case "latte":
-//           totalPrice += 63.00;
-//           alert("gran manera de comenzar el día; tu latte está siendo preparado :)");
-//           break;
-  
-//         case "cappuccino":
-//         case "capuchino":
-//         case "cappuchino":
-//         case "capuccino":
-//           totalPrice += 72.50;
-//           alert("espumoso, dulce y fresco; tu cappuccino está siendo preparado :)");
-//           break;
-  
-//         case "americano":
-//           totalPrice += 54.00;
-//           alert("recibiendo bien el día; tu americano está siendo preparado :)");
-//           break;
-  
-//         case "salir":
-//           alert("gracias por tu preferencia!");
-//           break;
-  
-//         default:
-//           alert("lo sentimos, de momento no tenemos disponible esa opción");
-//           break;
-//       }
-//     }
-  
-//     alert("tu total es de $" + totalPrice.toFixed(2) + " mxn. vuelve pronto!");
-//   }
-  
-//   coffeeOrder();
+function deleteProduct(e){
+    let cartItem;
+    if(e.target.tagName === "BUTTON"){
+        cartItem = e.target.parentElement;
+        cartItem.remove(); y
+    } else if(e.target.tagName === "I"){
+        cartItem = e.target.parentElement.parentElement;
+        cartItem.remove(); 
+    }
+
+    let products = getProductFromStorage();
+    let updatedProducts = products.filter(product => {
+        return product.id !== parseInt(cartItem.dataset.id);
+    });
+    localStorage.setItem('products', JSON.stringify(updatedProducts)); 
+    updateCartInfo();
+}
